94a85a660fb182e983ec83d88657691f9d8eac7a
Nutcake <Nusskuchen95@hotmail.de>
2025-04-10 20:51:14 +0200

    Remove ffmpeg_kit_flutter_audio dependency
    See https://github.com/Nutcake/ReCon/issues/48

--- lib/clients/audio_cache_client.dart
+++ lib/clients/audio_cache_client.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 
-import 'package:ffmpeg_kit_flutter_audio/ffmpeg_kit.dart';
+//import 'package:ffmpeg_kit_flutter_audio/ffmpeg_kit.dart';
 import 'package:http/http.dart' as http;
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
@@ -10,18 +10,19 @@ import 'package:recon/models/message.dart';
 
 class AudioCacheClient {
   final Future<Directory> _directoryFuture = getTemporaryDirectory();
-  final bool _isDarwin = Platform.isMacOS || Platform.isIOS;
+  //final bool _isDarwin = Platform.isMacOS || Platform.isIOS;
 
   Future<File> cachedNetworkAudioFile(AudioClipContent clip) async {
     final directory = await _directoryFuture;
     final fileName = basenameWithoutExtension(clip.assetUri);
     final file = File("${directory.path}/$fileName.ogg");
-    if (!await file.exists()) {
+    if (!file.existsSync()) {
       await file.create(recursive: true);
       final response = await http.get(Uri.parse(Aux.resdbToHttp(clip.assetUri)));
       ApiClient.checkResponseCode(response);
       await file.writeAsBytes(response.bodyBytes);
     }
+    /* This requires ffmpeg_kit_flutter_audio which is no longer maintained.
     if (_isDarwin) {
       final wavFile = File("${directory.path}/$fileName.wav");
       final wavFileExists = await wavFile.exists();
@@ -34,6 +35,7 @@ class AudioCacheClient {
       }
       return wavFile;
     }
+     */
     return file;
   }
 }
