40b678683e58faf7afe1c99b1fdac2742913ad9f
Delta <delta@deltawolf.net>
2026-06-20 11:05:32 -0700

    sort

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:intl/intl.dart';
+import 'package:just_audio_media_kit/just_audio_media_kit.dart';
 import 'package:logging/logging.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:provider/provider.dart';
@@ -24,7 +25,7 @@ import 'package:recon/models/sem_ver.dart';
 import 'package:recon/widgets/homepage.dart';
 import 'package:recon/widgets/login_screen.dart';
 import 'package:recon/widgets/update_notifier.dart';
-import 'package:just_audio_media_kit/just_audio_media_kit.dart';
+
 import 'models/authentication_data.dart';
 
 void main() async {
